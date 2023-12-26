dc2a282aad33c57872b8d7f0d23aa0ee7da53a53
idk3245 <149644187+idk3245@users.noreply.github.com>
2023-12-26 20:23:36 +0700

    Create calculatorV1.py

---
+++ calculatorV1.py
@@ -0,0 +1,16 @@
+prodolzhit = 'yes'
+while prodolzhit == 'yes':
+    f_num = float(input("введите первое число "))
+    oper = input("введите операцию ")
+    sec_num = float(input("введите второе число "))
+    if oper == '+':
+        print(f_num + sec_num)
+    elif oper == '-':
+        print(f_num - sec_num)
+    elif oper == '/':
+        print(f_num / sec_num)
+    elif oper == '*':
+        print(f_num * sec_num)
+    else:
+        print("Error")
+    prodolzhit = input("введите 'yes' чтоб продолжить, или что-то другое, что выйти ")
