83ab823f608cf632e8221ede8f643923c3581972
ikr3245 <149649222+ikr3245@users.noreply.github.com>
2023-12-27 12:28:37 +0700

    Update calculatorV1.py

--- calculatorV1.py
+++ calculatorV1.py
@@ -3,11 +3,18 @@ import tkinter as tk
 def calculate():
     try:
         num1 = float(entry_num1.get())
-@@ -14,7 +15,17 @@ def calculate():
+        num2 = float(entry_num2.get())
+        operation = entry_operation.get()
+
+        if operation not in ['+', '-', '*', '/']:
+            result_var.set("Неверная операция. Можно использовать '+', '-', '*' или '/'")
+            return
+
+        if operation == '/' and num2 == 0:
+            result_var.set("Error")
             return
 
         result = perform_calculation(num1, num2, operation)
-        result_text = f"Результат: {result}"
 
         last_digit = result % 10
 
@@ -18,33 +25,45 @@ def calculate():
         else:
             color_signature = "красный"
 
-        result_text = f("Результат: {result}, подпись цвета: {color_signature}")
+        result_text = f"Результат: {result}, подпись цвета: {color_signature}"
         result_var.set(result_text)
 
     except ValueError:
-@@ -31,8 +42,9 @@ def perform_calculation(a, b, operation):
+        result_var.set("Ошибка ввода. Вводите целые числа.")
+
+def perform_calculation(a, b, operation):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+    elif operation == '/':
         return a / b
 
 root = tk.Tk()
-root.title("Калькулятор без цветов")
 root.title("Калькулятор с подписью цвета")
 
-# Ввод чисел и операции
 label_num1 = tk.Label(root, text="Введите первое число:")
 label_num1.pack()
 entry_num1 = tk.Entry(root)
 entry_num1.pack()
+
 label_operation = tk.Label(root, text="Введите операцию (+, -, *, /):")
 label_operation.pack()
 entry_operation = tk.Entry(root)
 entry_operation.pack()
+
 label_num2 = tk.Label(root, text="Введите второе число:")
 label_num2.pack()
 entry_num2 = tk.Entry(root)
 entry_num2.pack()
+
 calculate_button = tk.Button(root, text="Выполнить подсчёт", command=calculate)
 calculate_button.pack()
+
 result_var = tk.StringVar()
 result_label = tk.Label(root, textvariable=result_var)
 result_label.pack()
+
 root.mainloop()
